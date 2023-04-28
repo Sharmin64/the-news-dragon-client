23399fc8e94bbcb02745b554f1ae3ae24a9f2d67
Sharmin64 <lizaeatsbanana@gmail.com>
2023-04-29 02:07:32 +0600

    login and register page created

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,22 +1,39 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
 import Category from "../pages/Home/Category/Category";
 import NewsLayouts from "../layouts/NewsLayouts";
 import News from "../pages/News/News/News";
+import LoginLayout from "../layouts/LoginLayout";
+import Login from "../pages/Login/Login/Login";
+import Register from "../pages/Login/Register/Register";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Main></Main>,
+    path: '/',
+    element: <LoginLayout></LoginLayout>,
     children: [
       {
-        path: "/",
-        element: <Category></Category>,
-        loader: ()=> fetch('http://localhost:8000/news')
+        path: '/',
+        element: <Navigate to='/category/0'></Navigate>
+    },
+      {
+        path: 'login',
+        element: <Login></Login>
       },
       {
-        path: '/category/:id',
+        path: 'register',
+        element: <Register></Register>
+      }
+    ]
+  },
+  {
+    path: "/category",
+    element: <Main></Main>,
+    children: [
+    
+      {
+        path: ':id',
         element: <Category></Category>,
         loader:({params})=>fetch(`http://localhost:8000/categories/${params.id}`)
       }
