fc504054898129bf89c929e663a09d01c483371e
Sharmin64 <lizaeatsbanana@gmail.com>
2023-04-29 01:37:43 +0600

    navigation bar created

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,9 +1,14 @@
 import React, { createContext } from 'react';
 import { getAuth } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
-export const AuthContext=createContext(null)
-const AuthProvider = ({children}) => {
-  const authInfo={}
+export const AuthContext = createContext(null)
+const auth=getAuth(app)
+const AuthProvider = ({ children }) => {
+  const user= {displayName: 'Chanachur Khan'}
+  const authInfo = {
+    user
+  }
   return (
     <AuthContext.Provider value={authInfo}>
       {children}
